94c17121043acc2117ccdb6a0f46f18f4d87eb2e
Morales Tavera Jonatan Arturo <johnta2610@hotmail.com>
2021-07-30 15:23:34 -0500

    v2. Calculaator
    Agregate new validations in app

--- Calculadora/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.example.calculator.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
@@ -16,7 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.tvOperation.addTextChangedListener { charSecuence ->
+            if (canReplaceOperator(charSecuence.toString())){
+                val lenght = binding.tvOperation.text.length
+                val newOperation = binding.tvOperation.text.toString().substring(0, lenght - 2) +
+                        binding.tvOperation.text.toString().substring(lenght - 1)
+                binding.tvOperation.text = newOperation
+            }
+        }
     }
+
+    private fun canReplaceOperator(charSecuence: CharSequence): Boolean {
+        if (charSecuence.length < 2) return false
+        val lastElement = charSecuence[charSecuence.length - 1].toString()
+        val penultimateElement = charSecuence[charSecuence.length - 2].toString()
+        return (lastElement == OPERATOR_MULTI ||
+                lastElement == OPERATOR_DIV ||
+                lastElement == OPERATOR_SUM) &&
+                (penultimateElement == OPERATOR_MULTI ||
+                penultimateElement == OPERATOR_DIV ||
+                penultimateElement == OPERATOR_SUM||
+                penultimateElement == OPERATOR_REST)
+    }
+
     fun onClickButton(view: View){
         val valueStr = (view as Button).text.toString()
         when(view.id){
@@ -32,7 +54,20 @@ class MainActivity : AppCompatActivity() {
                 binding.tvResult.text = ""
             }
             R.id.btnResult -> {
-                tryResolve(binding.tvOperation.text.toString())
+                tryResolve(binding.tvOperation.text.toString(), true)
+            }
+            R.id.btnMul,
+            R.id.btnDiv,
+            R.id.btnSum,
+            R.id.btnMin -> {
+                tryResolve(binding.tvOperation.text.toString(), false)
+                val operator = valueStr
+                val operation = binding.tvOperation.text.toString()
+                addOperator(operator, operation)
+                }
+            R.id.btnPoint -> {
+                val operation = binding.tvOperation.text.toString()
+                addPoint(valueStr, operation)
             }
             else -> {
                 binding.tvOperation.append(valueStr)
@@ -40,29 +75,118 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun tryResolve(operationRef: String) {
-        val operator = getOperator(operationRef)
+    private fun addPoint(pointStr: String, operation: String) {
+        if (!operation.contains(OPERATOR_POINT)){
+                binding.tvOperation.append(pointStr)
+        }else{
+            val operator = getOperator(operation)
+
+            var values = arrayOfNulls<String>(0)
+            if (operator != OPERATOR_NULL){
+                if (operator == OPERATOR_REST){
+                    val index = operation.lastIndexOf(OPERATOR_REST)
+                    if (index < operation.length-1){
+                        values = arrayOfNulls(2)
+                        values[0] = operation.substring(0, index)
+                        values[1] = operation.substring(index + 1)
+                    }else{
+                        values = arrayOfNulls(1)
+                        values[0] = operation.substring(0, index)
+                    }
+                }else{
+                    values = operation.split(operator).toTypedArray()
+                }
+            }
+            if (values.size > 0){
+                val numberOne = values[0]
+                if (values.size > 1){
+                    val numberTwo = values[1]
+                    if (numberOne != null) {
+                        if (numberTwo != null) {
+                            if (numberOne.contains(OPERATOR_POINT) && !numberTwo.contains(OPERATOR_POINT)){
+                                binding.tvOperation.append(pointStr)
+                            }else{
+                                if(numberOne.contains(OPERATOR_POINT)){
+                                    binding.tvOperation.append(pointStr)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addOperator(operator: String, operation: String) {
+            val lastElement = if (operation.isEmpty()) ""
+            else operation.substring(operation.length - 1)
+        if (operator == OPERATOR_REST){
+            if (operation.isEmpty() || lastElement != OPERATOR_REST && lastElement != OPERATOR_POINT){
+                binding.tvOperation.append(operator)
+            }
+        }else{
+            if (!operation.isEmpty() && lastElement != OPERATOR_POINT){
+                binding.tvOperation.append(operator)
+            }
+        }
+    }
+
+    private fun tryResolve(operationRef: String, isFromResolve: Boolean) {
+        if (operationRef.isEmpty())return
+        var operation = operationRef
+        if (operation.contains(OPERATOR_POINT) && operation.lastIndexOf(OPERATOR_POINT) == operation.length - 1){
+            operation = operation.substring(0, operation.length - 1)
+        }
+        val operator = getOperator(operation)
         var values = arrayOfNulls<String>(0)
-        values = operationRef.split(operator).toTypedArray()
-        val numberOne = values[0]!!.toDouble()
-        val numberTwo = values[1]!!.toDouble()
-        binding.tvResult.text = getResult(numberOne, operator, numberTwo).toString()
+        if (operator != OPERATOR_NULL){
+            if (operator == OPERATOR_REST){
+                val index = operation.lastIndexOf(OPERATOR_REST)
+                if (index < operation.length-1){
+                    values = arrayOfNulls(2)
+                    values[0] = operation.substring(0, index)
+                    values[1] = operation.substring(index + 1)
+                }else{
+                    values = arrayOfNulls(1)
+                    values[0] = operation.substring(0, index)
+                }
+            }else{
+                values = operation.split(operator).toTypedArray()
+            }
+        }
+        if (values.size > 1){
+            try {
+                val numberOne = values[0]!!.toDouble()
+                val numberTwo = values[1]!!.toDouble()
+                binding.tvResult.text = getResult(numberOne, operator, numberTwo).toString()
+                if (binding.tvResult.text.isNotEmpty() && !isFromResolve){
+                    binding.tvOperation.text = binding.tvResult.text
+                }
+            } catch (e: NumberFormatException){
+                if (isFromResolve) showMessage()
+            }
+        }else{
+            if (isFromResolve && operator != OPERATOR_NULL) showMessage()
+        }
     }
 
     private fun getOperator(operation: String): String {
-        var operator = ""
+        var operator: String
         if (operation.contains(OPERATOR_MULTI)){
             operator = OPERATOR_MULTI
         } else if (operation.contains(OPERATOR_DIV)){
             operator = OPERATOR_DIV
-        }else if (operation.contains(OPERATOR_REST)){
-            operator = OPERATOR_REST
         }else if (operation.contains(OPERATOR_SUM)){
             operator = OPERATOR_SUM
+        }else{
+            operator = OPERATOR_NULL
+        }
+        if (operator == OPERATOR_NULL && operation.lastIndexOf(OPERATOR_REST) > 0){
+            operator = OPERATOR_REST
         }
         return operator
     }
-    fun getResult(numberOne: Double, operator: String, numberTwo: Double): Double{
+    private fun getResult(numberOne: Double, operator: String, numberTwo: Double): Double{
         var result = 0.0
         when(operator){
             OPERATOR_MULTI -> result = numberOne * numberTwo
@@ -71,6 +195,9 @@ class MainActivity : AppCompatActivity() {
             OPERATOR_REST -> result = numberOne - numberTwo
         }
         return result
+    }
+    private fun showMessage(){
+        Snackbar.make(binding.root, getString(R.string.message_exp_incorrect), Snackbar.LENGTH_SHORT).setAnchorView(binding.llTop).show()
     }
     companion object{
         const val OPERATOR_MULTI = "*"
